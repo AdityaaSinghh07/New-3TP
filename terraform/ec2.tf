358b421ff36ca5e91823ab066752983a391bbb7f
AdityaaSinghh07 <aditya.singh@codelogicx.com>
2026-05-29 13:58:10 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0c2af51e265bd5e0e"
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = file("user_data.sh")
+}
